2ac636125fb5d155c2716222f31f92f42ee1e385
Ricky-Xu-UOW <71579588+Ricky-Xu-UOW@users.noreply.github.com>
2020-10-05 04:48:08 +1100

    got myCollections done

--- OceanArchiveRevamp/app.tsx
+++ OceanArchiveRevamp/app.tsx
@@ -15,6 +15,7 @@ import Header from './components/header';
 import Home from './components/home';
 import Map from './components/map';
 import MyItems from './components/myItems';
+import MyCollections from './components/myCollections';
 import MyAnnouncements from './components/myAnnouncements';
 import ItemCollectionPage from './components/itemCollectionPage';
 import CreateItem from './components/createItem';
@@ -61,6 +62,7 @@ class Homepage extends React.Component {
                     <Route path="/home" component={Home} />
                     <Route path="/map" component={Map} />
                     <Route path="/myItems" component={MyItems} />
+                    <Route path="/myCollections" component={MyCollections} />
                     <Route path="/myAnnouncements" component={MyAnnouncements} />
                     <Route path="/itemPage" component={ItemCollectionPage} />
                     <Route path="/createItem" component={CreateItem} />
