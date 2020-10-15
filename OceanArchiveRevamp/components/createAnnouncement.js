a452d3d2eac3063d7a32e73ff92e157dd03731b9
Jiaqi Guan <jg081@uowmail.edu.au>
2020-10-15 23:42:15 +1100

    createAnnouncement

---
+++ OceanArchiveRevamp/components/createAnnouncement.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require('react');
+var ReactDOM = require('react-dom');
+const Constant = require("../constants");
+const reactstrap_1 = require("reactstrap");
+const react_select_1 = require("react-select");
+const google_map_react_1 = require("google-map-react");
+let Draggable = require('react-draggable');
