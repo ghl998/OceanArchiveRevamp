380acb30dd7926d55d40d9f62c0d71d9608d181f
Ricky-Xu-UOW <71579588+Ricky-Xu-UOW@users.noreply.github.com>
2020-10-24 16:34:41 +1100

    create a new one

--- OceanArchiveRevamp/components/createCollection.js
+++ OceanArchiveRevamp/components/createCollection.js
@@ -7,7 +7,6 @@ const react_router_dom_1 = require("react-router-dom");
 const reactstrap_1 = require("reactstrap");
 const react_select_1 = require("react-select");
 const google_map_react_1 = require("google-map-react");
-const react_router_dom_1 = require("react-router-dom");
 let Draggable = require('react-draggable');
 class DetailsPage extends React.Component {
     constructor(props) {
@@ -198,31 +197,11 @@ class AddItemPage extends React.Component {
     }
     render() {
         return (React.createElement("div", { className: 'createItemPage' },
-            "Regions",
-            React.createElement("hr", null),
-            React.createElement(reactstrap_1.FormGroup, null,
-                React.createElement(reactstrap_1.Label, { for: 'oceans' }, "Ocean Region/s (optional)"),
-                React.createElement(react_select_1.default, { className: 'react-select-contianer', classNamePrefix: 'react-select', options: Constant.oceans, isMulti: true, isSearchable: true })),
-            React.createElement(reactstrap_1.FormGroup, null,
-                React.createElement(reactstrap_1.Label, { for: 'countries' }, "Country/s (optional)"),
-                React.createElement(react_select_1.default, { className: 'react-select-contianer', classNamePrefix: 'react-select', options: Constant.countries, value: Constant.countries.value, isMulti: true, isSeachable: true })),
-            React.createElement("div", { style: { height: '50px' } }),
-            "Legal",
-            React.createElement("hr", null),
-            React.createElement(reactstrap_1.FormGroup, null,
-                React.createElement(reactstrap_1.Label, { for: 'license' }, "License"),
-                React.createElement(reactstrap_1.Input, { type: 'select', name: 'license' },
-                    React.createElement("option", null, "CC BY (Least Restrictive)"),
-                    React.createElement("option", null, "CC BY-SA"),
-                    React.createElement("option", null, "CC BY-ND"),
-                    React.createElement("option", null, "CC BY-NC"),
-                    React.createElement("option", null, "CC BY-NC-SA"),
-                    React.createElement("option", null, "CC BY-NC-ND (Most Restrictive CC)"),
-                    React.createElement("option", null, "Ocean Archive (Most Restrictive)"))),
-            React.createElement(reactstrap_1.FormGroup, null,
-                React.createElement(reactstrap_1.Label, { for: 'copyr' }, "Copyright Owner (optional)"),
-                React.createElement(reactstrap_1.Input, { type: 'text', name: 'copyr' }))));
+            React.createElement(react_router_dom_1.NavLink, { className: 'buttonBig', to: "/createItem" }, "ADD NEW ITEM"),
+            React.createElement("div", { style: { height: '24px' } }),
+            React.createElement(react_router_dom_1.NavLink, { className: 'buttonBig', to: "/createItem" }, "ADD NEW ITEM")));
     }
+    
 }
 
 class AddCollectionPage extends React.Component {
@@ -236,7 +215,8 @@ class AddCollectionPage extends React.Component {
         }
         render() {
         return (React.createElement("div", { className: 'createItemPage' },
-            React.createElement(react_router_dom_1.NavLink, { className: 'buttonSmall', to: "/createCollection" }, "ADD NEW ITEM")));
+            React.createElement(react_router_dom_1.NavLink, { className: 'buttonSmall', to: "/createCollection" }, "ADD NEW ITEM"),
+            React.createElement(react_router_dom_1.NavLink, { className: 'button', to: "/createCollection" }, "ADD NEW ITEM")));
         }
 }
 
